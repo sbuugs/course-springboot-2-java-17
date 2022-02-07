7146f31211e2e4e80b23fec32d182322a23ddafd
sbuugs <ggubs0816@gmail.com>
2022-02-07 16:16:31 -0300

    H2 database, test profile, JPA

--- src/main/java/com/udemy/course/entities/User.java
+++ src/main/java/com/udemy/course/entities/User.java
@@ -3,24 +3,32 @@ package com.udemy.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
-	private String fone;
+	private String phone;
 	private String password;
 	
 	public User() {
 	}
 
-	public User(Long id, String name, String email, String fone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
-		this.fone = fone;
+		this.phone = phone;
 		this.password = password;
 	}
 
@@ -48,12 +56,12 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
-	public String getFone() {
-		return fone;
+	public String getPhone() {
+		return phone;
 	}
 
-	public void setFone(String fone) {
-		this.fone = fone;
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
 	public String getPassword() {
